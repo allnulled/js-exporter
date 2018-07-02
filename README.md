a9764bc6de637c7b65e9df3faf8cf68bd6761a2b
allnulled <todosanulados@gmail.com>
2018-07-02 10:55:37 +0200

    Updated README file to explain the last method.

--- README.md
+++ README.md
@@ -168,21 +168,26 @@ but it automatically reads and writes the implied files. It returns the code exp
 
 ----
 
-#### **`JsExporter.getMetadataFromCode(code:String, dstFile:String, options:Object)`**
+#### **`JsExporter.getMetadataFromCode(code:String, optionsBeautification:Object, token1:String, token1group1:Number, token2:String)`**
 
 
 **Type:** `{Static method}`
 
-**Parameter:** `{String} srcFile`. **Required**. File to be exported.
+**Parameter:** `{String} code`. **Required**.
 
-**Parameter:** `{String} dstFile`. **Required**. File to which dump the exportation into.
+**Parameter:** `{Object} optionsBeautification`. **Optional**. The options to be passed to [UglifyES](https://github.com/mishoo/UglifyJS2). By default: `{}`.
 
-**Parameter:** `{Object} options`. **Optional**. The options to be passed to [UglifyES](https://github.com/mishoo/UglifyJS2).
+**Parameter:** `{String} token1`. **Optional**. The text of the regular expression that defines the name of the exported module for the browsers. By default: `"(^|\n)[\t\r\n ]*\/\/ *\>\> *export +module +to +([a-zA-Z$_][a-zA-Z0-9$_]*) *(\n|$)"`.
+
+**Parameter:** `{Number} token1group1`. **Optional**. The number of the group, from the previous regular expression match, that contains the name of the exported module. By default: `2`.
+
+**Parameter:** `{String} token2`. **Optional**. The text of the regular expression that precedes the data exported by the module. By default: `"(^|\n)[\t\r\n ]*\/\/ *\>\> *export +module[\t\r\n ]+module\.exports *= *"`.
 
 **Return:** `{String} codeExported`. The code once wrapped and beautified.
 
-**Description:** This method does the same as the `JsExporter.exportCode(code:String, options:Object)`,
-but it automatically reads and writes the implied files. It returns the code exported too.
+**Description:** This method is an internal feature, but the developer can use it too. It specifies the parameters of
+the exportation, and obtains the exported code. The other methods call to this method under the hood. Using this method, 
+one can customize the tokens that define both, the **name of the module** (used as global variable in browsers) and the **data of the module**.
 
 
 
--- src/js-exporter.js
+++ src/js-exporter.js
@@ -155,15 +155,18 @@ class JsExporter {
    * 
    * ----
    * 
-   * #### **`JsExporter.getMetadataFromCode(code:String, dstFile:String, options:Object)`**
+   * #### **`JsExporter.getMetadataFromCode(code:String, optionsBeautification:Object, token1:String, token1group1:Number, token2:String)`**
    * 
    * @type `{Static method}`
-   * @parameter `{String} srcFile`. **Required**. File to be exported.
-   * @parameter `{String} dstFile`. **Required**. File to which dump the exportation into.
-   * @parameter `{Object} options`. **Optional**. The options to be passed to [UglifyES](https://github.com/mishoo/UglifyJS2).
+   * @parameter `{String} code`. **Required**.
+   * @parameter `{Object} optionsBeautification`. **Optional**. The options to be passed to [UglifyES](https://github.com/mishoo/UglifyJS2). By default: `{}`.
+   * @parameter `{String} token1`. **Optional**. The text of the regular expression that defines the name of the exported module for the browsers. By default: `"(^|\n)[\t\r\n ]*\/\/ *\>\> *export +module +to +([a-zA-Z$_][a-zA-Z0-9$_]*) *(\n|$)"`.
+   * @parameter `{Number} token1group1`. **Optional**. The number of the group, from the previous regular expression match, that contains the name of the exported module. By default: `2`.
+   * @parameter `{String} token2`. **Optional**. The text of the regular expression that precedes the data exported by the module. By default: `"(^|\n)[\t\r\n ]*\/\/ *\>\> *export +module[\t\r\n ]+module\.exports *= *"`.
    * @return `{String} codeExported`. The code once wrapped and beautified.
-   * @description This method does the same as the `JsExporter.exportCode(code:String, options:Object)`,
-   * but it automatically reads and writes the implied files. It returns the code exported too.
+   * @description This method is an internal feature, but the developer can use it too. It specifies the parameters of
+   * the exportation, and obtains the exported code. The other methods call to this method under the hood. Using this method, 
+   * one can customize the tokens that define both, the **name of the module** (used as global variable in browsers) and the **data of the module**.
    * 
    */
   static getMetadataFromCode(
